e55a7510e86fd94e4b9d9421df74c0bb5c1ba177
Evan <evanz1215@gmail.com>
2024-04-16 22:20:29 +0800

    表單加上驗證規則

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -26,8 +26,11 @@ class _NewItemState extends State<NewItem> {
                   label: Text('Name'),
                 ),
                 validator: (value) {
-                  //驗證
-                  return "Demo...";
+                  // 表單驗證
+                  if (value == null || value.isEmpty || value.trim().length <= 1 || value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters.';
+                  }
+                  return null;
                 },
               ), // instead of TextField()
               Row(
@@ -39,6 +42,13 @@ class _NewItemState extends State<NewItem> {
                         label: Text('Quantity'),
                       ),
                       initialValue: "1",
+                      validator: (value) {
+                        // 表單驗證
+                        if (value == null || value.isEmpty || int.tryParse(value) == null || int.tryParse(value)! < 50) {
+                          return 'Must be a valid, positive number.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(width: 8),
@@ -61,6 +71,20 @@ class _NewItemState extends State<NewItem> {
                           ),
                         )
                     ], onChanged: (value) {}),
+                  )
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Add Item'),
                   )
                 ],
               )
